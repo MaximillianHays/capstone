a5b465d7be5b612001103a1461571f9ad84e5917
maxhays <maxhays@uw.edu>
2025-02-27 22:03:30 -0800

    Disable Logging

--- game.js
+++ game.js
@@ -83,14 +83,14 @@ function log(event) {
 	if (logs.length == 25) sendLogs();
 }
 function sendLogs() {
-	if (!logs.length) return;
-	if (userID == "test") return;
-	fetch("https://t7vszikxbycghcwfasvys46jhm0zpchl.lambda-url.us-west-2.on.aws/", {
-		body: JSON.stringify({logs, uid: userID, sid: getSessionID(), version, iVersion: +localStorage.getItem("initialVersion"), area: innerHeight * innerWidth}),
-		method: "POST",
-		keepalive: true
-	});
-	logs = [];
+	// if (!logs.length) return;
+	// if (userID == "test") return;
+	// fetch("https://t7vszikxbycghcwfasvys46jhm0zpchl.lambda-url.us-west-2.on.aws/", {
+	// 	body: JSON.stringify({logs, uid: userID, sid: getSessionID(), version, iVersion: +localStorage.getItem("initialVersion"), area: innerHeight * innerWidth}),
+	// 	method: "POST",
+	// 	keepalive: true
+	// });
+	// logs = [];
 }
 function startSession() {
 	log({
